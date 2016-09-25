2b5e0b10877c676daffbbe18dd959bada953da53
Turnerj <git@turnerj.com>
2016-09-25 16:52:57 +0930

    Removed redundant "flatten" fetch option (plus removing old gzip option)

--- TurnerSoftware.Sitemap/SitemapFetchOptions.cs
+++ TurnerSoftware.Sitemap/SitemapFetchOptions.cs
@@ -20,18 +20,12 @@ namespace TurnerSoftware.Sitemap
         /// When true, if the type of sitemap is unknown, it will throw a <see cref="NotSupportedException" />. Default is true.
         /// </summary>
         public bool ThrowExceptionOnUnknownType { get; set; }
-        /// <summary>
-        /// When true, flattens all found sitemaps from a sitemap index into a master sitemap. Default is false.
-        /// </summary>
-        public bool FlattenSitemap { get; set; }
 
         public SitemapFetchOptions()
         {
             FetchInnerSitemaps = true;
-            DecompressGZipSitemaps = true;
             ApplyDomainRestrictions = true;
             ThrowExceptionOnUnknownType = true;
-            FlattenSitemap = false;
         }
     }
 }

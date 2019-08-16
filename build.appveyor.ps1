2beb269135f05b567a880eab35a8e2732c0e3cd7
Turnerj <git@turnerj.com>
2019-08-16 15:57:04 +0930

    Updated build process

---
+++ build.appveyor.ps1
@@ -0,0 +1,16 @@
+[CmdletBinding(PositionalBinding=$false)]
+param(
+    [bool] $IsTagBuild
+)
+
+
+Write-Host "Initialising AppVeyor build..." -ForegroundColor "Magenta"
+
+if ($IsTagBuild) {
+	.\build.ps1 -CreatePackages $True
+}
+else {
+	.\build.ps1 -CheckCoverage $True
+}
+
+Exit $LastExitCode
